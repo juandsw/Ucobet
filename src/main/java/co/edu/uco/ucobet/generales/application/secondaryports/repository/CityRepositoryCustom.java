ec310d37b3fee192b56e19eccdca4851ee786817
juandsw <juandavidgallegohernandez@gmail.com>
2024-11-07 03:34:13 -0500

    ...

--- src/main/java/co/edu/uco/ucobet/generales/application/secondaryports/repository/CityRepositoryCustom.java
+++ src/main/java/co/edu/uco/ucobet/generales/application/secondaryports/repository/CityRepositoryCustom.java
@@ -2,7 +2,6 @@ package co.edu.uco.ucobet.generales.application.secondaryports.repository;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import co.edu.uco.ucobet.generales.application.secondaryports.entity.CityEntity;
@@ -10,7 +9,8 @@ import co.edu.uco.ucobet.generales.application.secondaryports.entity.CityEntity;
 @Repository
 public interface CityRepositoryCustom {
 	
-	//@Query("SELECT c FROM City c WHERE c.name = :cityName AND c.state.id = :stateId")
 	List<CityEntity> SelectByFilter(CityEntity filter);
+
+
 
 }
